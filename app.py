66182d749e216e54227cb929240fd369f28729fd
Wafi FR <167063534+Wafi-FR@users.noreply.github.com>
2025-07-10 01:18:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/18mCj5W06jql2HfGV782KF1xLm4tzVbZp
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
